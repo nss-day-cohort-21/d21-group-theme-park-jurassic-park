84a8ba0fc49081ce8bf3d5a96380c1cf51cc3fa3
Michael Lindstrom <michaellindstr@gmail.com>
2017-08-15 13:37:59 -0500

    made app layout / grid area

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 'use strict';
 
+var handlebars = require('hbsfy/runtime');
 var template = require('./templates');
+var areaGrid = require('../templates/areaGrid.hbs');
 
 template.loadNavbar();
+
 
 var Park = {
   loadAreas: function(){
@@ -10,6 +13,20 @@ var Park = {
       url: "https://android-chat-app-c66de.firebaseio.com/areas.json"
     }).done(function(data){
       console.log(data);
+      $('#main-content').append(
+        `<div class=row>
+          <div class=col-4>
+          </div>
+          <div class=col-8>
+            <div class=row>
+            </div>
+          </div>
+        </div>`);
+
+        $(data).each((index, item) =>{
+          $('.col-8 > .row').append(`<div class="col-4">${item.name}</div>`);
+        });
+
     });
   }
 };
