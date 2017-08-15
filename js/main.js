b6f36f05f5159f53b60a61e734137f547423fa15
DanielGreene101 <d.b.greene101@gmail.com>
2017-08-15 13:37:41 -0500

    images added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 'use strict';
 
 var template = require('./templates');
+var areaGrid = require('../templates/areaGrid.hbs');
 
 template.loadNavbar();
+
+var Park = {
+  loadAreas: function(){
+    $.ajax({
+      url: "https://android-chat-app-c66de.firebaseio.com/areas.json"
+    }).done(function(data){
+      console.log(data);
+      $('#main-content').append(areaGrid(data));
+    });
+  }
+};
+
+Park.loadAreas();
 
