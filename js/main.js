2ea174cbda748ac58ed76e95dd0b9ad43c63b280
Michael Lindstrom <michaellindstr@gmail.com>
2017-08-15 10:24:34 -0500

    load areas

--- js/main.js
+++ js/main.js
@@ -4,3 +4,14 @@ var template = require('./templates');
 
 template.loadNavbar();
 
+var Park = {
+  loadAreas: function(){
+    $.ajax({
+      url: "https://android-chat-app-c66de.firebaseio.com/areas.json"
+    }).done(function(data){
+      console.log(data);
+    });
+  }
+};
+
+Park.loadAreas();
