e65a434ad606c5116a58c61de1de4ec1eecf21d8
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-08-15 11:30:04 -0500

    some work on data loader

--- js/data_loader.js
+++ js/data_loader.js
@@ -1,34 +1,29 @@
 'use strict';
-var Park = {};
-let _attractionsArr = [];
+var Park = {
+
+// attractionsArr: [],
 
 //load attractions from firebase
-Park.loadAttractionsList = () => {
-	return new Promise((resolve, reject) => {
+loadAttractionsList: function () {
 		$.ajax ({
 			url: 'https://android-chat-app-c66de.firebaseio.com/attractions.json'
 		}).done(function(data){
-			resolve(data);
+			this.createArray(data);
 
 		});
+},
+
+//create attractions array w/ attractionsList
+
+createArray: function (data) {
+	let attractionsArr = [];
+	data.forEach((item)=>{
+		attractionsArr.push(item.name);
 	});
+			// console.log("attractions array: ", arr);
+	return attractionsArr;
+}
 };
+console.log(Park.loadAttractionsList());
 
-//return attractions objects that match search string
-//CHANGE 'SWISS FAMILY TREEHOUSE' TO SEARCH.VAL()
-Park.loadAttractionsList()
-	.then(function(data){
-		let $a = $('<a></a>');
-		data.forEach((item, index)=>{
-			if (item.name === 'Swiss Family Treehouse') {
-				_attractionsArr.push(item);
-				console.log("a", $a);
-				$a.attr('href', '#');
-				$a.text(item.name);
-			}
-			$('body').append($a);
-		});
-		console.log("attractions", _attractionsArr);
-	})
-
-// module.exports = Park;
+module.exports = Park;
