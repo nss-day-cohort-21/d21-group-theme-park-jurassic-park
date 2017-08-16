bfb59ce033a3c026943f1efbb9e4f55548d808c9
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-08-16 10:56:22 -0500

    search functionality started using fuse.js

--- js/search.js
+++ js/search.js
@@ -1,2 +1,35 @@
-var fuse = new Fuse();
-console.log("fuse is ", fuse );
+
+let Park = require('./data_loader.js');
+let Search = {};
+let attractionsArr = [];
+
+//fuse search parameters
+var options = {
+  shouldSort: true,
+  threshold: 0.1,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  keys: [
+  	'name'
+  ]
+};
+
+Park.attractionsCall().then(function(resolve){
+	attractionsArr = resolve;
+});
+
+$('#user-input').keyup(function(){
+	let searchText = $('#user-input').val();
+	let fuse = new Fuse(attractionsArr, options);
+	let result = fuse.search(searchText);
+	console.log(result);
+});
+
+//1.  get attractions array (attractionsCall() resolve is array)
+//2. create fuse object w/ array
+//3. on key up, call fuse,search...
+//4. ... print result to sidebar
+
+
