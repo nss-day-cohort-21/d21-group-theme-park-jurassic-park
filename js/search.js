4b7493e23bb0eaabba1e317e630372bcda0ff59e
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-08-16 11:47:18 -0500

    search results printing to DOM

--- js/search.js
+++ js/search.js
@@ -21,11 +21,26 @@ Park.attractionsCall().then(function(resolve){
 });
 
 $('#user-input').keyup(function(){
-	let searchText = $('#user-input').val();
-	let fuse = new Fuse(attractionsArr, options);
-	let result = fuse.search(searchText);
-	console.log(result);
+    let searchText = $('#user-input').val();
+    let fuse = new Fuse(attractionsArr, options);
+    let result = fuse.search(searchText);
+    let namesList = '';
+    result.forEach((item)=>{
+    	namesList += item.name;
+    });
+	printResults(namesList);
 });
+
+printResults = function (result){
+	$('#searchResults').html('');
+    let content = "";
+    let newDiv = $('<div></div>');
+    newDiv.text(result);
+    $('#searchResults').append(newDiv);
+    console.log('new div test', result);
+};
+
+
 
 //1.  get attractions array (attractionsCall() resolve is array)
 //2. create fuse object w/ array
