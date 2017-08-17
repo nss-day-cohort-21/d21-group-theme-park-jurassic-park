aed3588390d6faa068fc11f7eefd30b463630c60
Gilberto Diaz <gdiaz@Gilbertos-MacBook-Pro.local>
2017-08-17 10:25:22 -0500

    workin on times

--- js/handlers.js
+++ js/handlers.js
@@ -2,11 +2,13 @@
 
 let dataLoader = require('./data_loader');
 let templates = require('./templates.js');
+var _ = require('../lib/node_modules/lodash/lodash.min');
 
 var Handlers = {
   loadAttractionsOnEnterKey: function() {
     $(document).keypress(function(event) {
-      if (event.keyCode === 13 && $('#user-input').is(':focus') && $('#user-input').val() !== '') {
+      if (event.keyCode === 13 && $('#user-input').is(':focus') &&
+        $('#user-input').val() !== '') {
         let userStr = $('#user-input').val();
         let capitalizedWord = _.startCase(_.toLower(userStr));
 
@@ -18,7 +20,8 @@ var Handlers = {
   },
   loadAttractionsOnClickArea: function() {
     $('.grid-row').click(function(event) {
-      templates.loadAttractionsByArea(dataLoader.attractionsCallByAreaId, event.target.id);
+      templates.loadAttractionsByArea(dataLoader.attractionsCallByAreaId,
+        event.target.id);
     });
   },
 
@@ -34,11 +37,20 @@ var Handlers = {
         $(imageElement).attr('style', `border: 3px solid #${color}`);
       });
     });
-  }
+  },
+
+  addTimeFrame: function(attractionCall) {
+    attractionCall().then(function(data) {
+      _.forEach(data, function(item) {
+        console.log('Data: ', item, item.times);
+      });
+    });
+  },
 };
 
 Handlers.loadAttractionsOnEnterKey();
 Handlers.loadAttractionsOnClickArea();
 Handlers.addBorderColor(dataLoader.areasCall);
+Handlers.addTimeFrame(dataLoader.attractionsCall);
 
 module.exports = Handlers;
